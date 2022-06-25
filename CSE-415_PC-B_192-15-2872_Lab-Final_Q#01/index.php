605c4867d200cd41759b0933ab92855d5e132ac0
itsMHmoon <mhmoon.info@gmail.com>
2022-06-25 14:52:11 +0600

    initial commit

---
+++ CSE-415_PC-B_192-15-2872_Lab-Final_Q#01/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Database</title>
+</head>
+<body>
+	<center>
+		<h1>Storing data in Database</h1>
+		<form action="insert.php" method="post">
+			
+<p>
+			<label for="Name">Name:</label>
+			<input type="text" name="name" id="Name">
+			</p>
+
+			
+<p>
+			<label for="Id">ID:</label>
+			<input type="text" name="id" id="Id">
+			</p>
+
+			
+<p>
+			<label for="Age">Age:</label>
+			<input type="text" name="age" id="Age">
+			</p>
+
+			
+<p>
+			<label for="Username">Username:</label>
+			<input type="text" name="username" id="Username">
+			</p>
+
+			
+<p>
+			<label for="Password">Password:</label>
+			<input type="password" name="password" id="Password">
+			</p>
+
+			<input type="submit" value="Submit">
+		</form>
+	</center>
+</body>
+</html>
---
+++ CSE-415_PC-B_192-15-2872_Lab-Final_Q#01/insert.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <title>Insert Page</title>
+</head>
+ 
+<body>
+    <center>
+        <?php
+ 
+        $servername='localhost';
+        $username='root';
+        $password='';
+        $dbname='data';
+        $conn = mysqli_connect("localhost", "root", "", "data");
+         
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+         
+        $name =  $_REQUEST['name'];
+        $id = $_REQUEST['id'];
+        $age =  $_REQUEST['age'];
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+         
+        $sql = "INSERT INTO basicdata  VALUES ('$name',
+            '$id','$age','$username','$password')";
+         
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>";
+ 
+            echo nl2br("\n$name\n $id\n "
+                . "$age\n $username\n $password");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+         
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+ 
+</html>
